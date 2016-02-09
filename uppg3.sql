5bec1407c8d1fcee39915a09d5cb083b2a6ece43
amanske <alexanderjanmanske@gmail.com>
2016-02-09 21:45:38 +0100

    Added new uppg3.sql

--- uppg3.sql
+++ uppg3.sql
@@ -1,5 +1,11 @@
-SELECT DISTINCT Mountain.mountains, count(gm.country)
-FROM Mountain, geo_mountain AS gm
-WHERE Mountain.mountains IS NOT NULL 
-    AND Mountain.name = gm.mountain
-GROUP BY Mountain.mountains
+SELECT mountainrange, count(country)
+	FROM
+		(SELECT DISTINCT mountainrange, country
+		FROM
+			(SELECT mountains AS mountainrange, count(Mountain) AS numberofmountains
+			FROM Mountain
+			WHERE mountains IS NOT NULL
+			GROUP BY mountains) foo, (SELECT name AS Mountainname, mountains FROM mountain WHERE mountains IS NOT NULL) bar, geo_mountain
+		WHERE mountains = mountainrange AND mountain = Mountainname AND numberofmountains > 2
+		) blabla
+	GROUP BY mountainrange
