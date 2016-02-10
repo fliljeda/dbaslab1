f4f9fba25c2c41b5a75005e5310d9b2b5d307264
Fredrik Liljedahl <fredrik.liljedahl@hotmail.com>
2016-02-10 14:33:31 +0100

    fixed

--- uppg3.sql
+++ uppg3.sql
@@ -1,11 +1,15 @@
 SELECT mountainrange, count(country)
+FROM
+	(SELECT DISTINCT mountainrange, country
 	FROM
-		(SELECT DISTINCT mountainrange, country
-		FROM
-			(SELECT mountains AS mountainrange, count(Mountain) AS numberofmountains
-			FROM Mountain
-			WHERE mountains IS NOT NULL
-			GROUP BY mountains) foo, (SELECT name AS Mountainname, mountains FROM mountain WHERE mountains IS NOT NULL) bar, geo_mountain
-		WHERE mountains = mountainrange AND mountain = Mountainname AND numberofmountains > 2
-		) blabla
-	GROUP BY mountainrange
+		(SELECT mountains AS mountainrange, count(Mountain) AS numberofmountains
+		FROM Mountain
+		WHERE mountains IS NOT NULL
+		GROUP BY mountains) foo, 
+        (SELECT name AS Mountainname, mountains 
+        FROM mountain 
+        WHERE mountains IS NOT NULL) bar, 
+        geo_mountain
+	WHERE mountains = mountainrange AND mountain = Mountainname AND numberofmountains > 2
+	) blabla
+GROUP BY mountainrange
--- uppg8.sql
+++ uppg8.sql
@@ -1,5 +1,11 @@
-SELECT country, POWER(pgro, 10) * pop AS tenyears, POWER(pgro, 25) * pop AS twentyfiveyears, POWER(pgro, 50) * pop AS fiftyyears, POWER(pgro, 100) * pop AS hundredyears
-FROM   (SELECT Country.name AS country, Country.population AS pop, (Population.population_growth+100)/100 AS pgro
-	FROM Population, Country
-	WHERE Country.code = Population.country) foo
+SELECT country,
+        POWER(pgro, 10) * pop AS tenyears,
+        POWER(pgro, 25) * pop AS twentyfiveyears,
+        POWER(pgro, 50) * pop AS fiftyyears,
+        POWER(pgro, 100) * pop AS hundredyears
+FROM   (SELECT Country.name AS country,
+            Country.population AS pop,
+            (Population.population_growth+100)/100 AS pgro
+	    FROM Population, Country
+        WHERE Country.code = Population.country) foo
 ORDER BY country
